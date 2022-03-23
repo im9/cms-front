cac0bdacc5f07bdb4f10ed2467424c6cd9f3ec66
t-nakamura <im9today@me.com>
2022-03-23 14:52:11 +0900

    add mdi icons

--- app.vue
+++ app.vue
@@ -3,8 +3,18 @@
     <v-app-bar app>
       <v-app-bar-title>CMS</v-app-bar-title>
     </v-app-bar>
+    <v-navigation-drawer
+      expand-on-hover
+      rail
+    >
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-home" title="Home" value="home"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
-      <nuxt-page></nuxt-page>
+      <NuxtLayout>
+        <NuxtPage></NuxtPage>
+      </NuxtLayout>
     </v-main>
   </v-app>
 </template>
